9c87ade82000b8fa03d2a92dd0db063195615c28
Keith Lockwood <kwlockwo@gmail.com>
2017-03-05 01:01:48 +1100

    Add dns cache ttl

--- bin/run_afl_player_loader.sh
+++ bin/run_afl_player_loader.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export CLASSPATH=/app/target/dflmngr.jar:/app/target/dependency/*
-java -classpath $CLASSPATH -Dnetworkaddress.cache.ttl=20 -Dnetworkaddress.cache.negative.ttl=0 net.dflmngr.handlers.AflPlayerLoaderHandler
+java -classpath $CLASSPATH net.dflmngr.handlers.AflPlayerLoaderHandler
