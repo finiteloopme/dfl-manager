df9cdeb8b9aa5af81ab29a7660eef4aa636f4b2b
Keith Lockwood <kwlockwo@gmail.com>
2017-03-17 12:12:40 +1100

    Fix job dates

--- src/main/java/net/dflmngr/scheduler/generators/ResultsJobGenerator.java
+++ src/main/java/net/dflmngr/scheduler/generators/ResultsJobGenerator.java
@@ -1,10 +1,14 @@
 package net.dflmngr.scheduler.generators;
 
-import java.text.SimpleDateFormat;
+//import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Calendar;
+//import java.util.Calendar;
 import java.util.Collections;
-import java.util.GregorianCalendar;
+//import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,8 +38,8 @@ public class ResultsJobGenerator {
 	DflRoundInfoService dflRoundInfoService;
 	AflFixtureService aflFixtureService;
 	
-	private static SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-YYYY");
-	private static SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a");
+	//private static SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-YYYY");
+	//private static SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a");
 	
 	public ResultsJobGenerator() {
 		//loggerUtils = new LoggingUtils("batch-logger", "batch.name", "ResultsJobGenerator");
@@ -89,29 +93,30 @@ public class ResultsJobGenerator {
 		
 		Collections.sort(aflGames);
 		
-		int currentGameDay = 0;
-		int previousGameDay = 0;
+		DayOfWeek currentGameDay = null;
+		DayOfWeek previousGameDay = null;
 		
-		Calendar startTimeCal = null;
+		ZonedDateTime gameStart = null;
 		
 		for(AflFixture game : aflGames) {
 			
 			loggerUtils.log("info", "AFL Fixture={}", game);
 			
-			startTimeCal = GregorianCalendar.from(game.getStart());
+			gameStart = game.getStart();
 			//startTimeCal = Calendar.getInstance();
 			//startTimeCal.setTime(game.getStart());
-			currentGameDay = startTimeCal.get(Calendar.DAY_OF_WEEK);
+			//currentGameDay = startTimeCal.get(Calendar.DAY_OF_WEEK);
+			currentGameDay = gameStart.getDayOfWeek();
 			
 			loggerUtils.log("info", "Current Game Day={}; Previous Game Day={};", currentGameDay, previousGameDay);
 			
 			if(currentGameDay != previousGameDay) {
-				if(currentGameDay == Calendar.SUNDAY || currentGameDay == Calendar.SATURDAY) {
-					loggerUtils.log("info", "Creating weekend run, start time={}", startTimeCal);
-					createWeekendSchedule(dflRound, startTimeCal);
+				if(currentGameDay == DayOfWeek.SUNDAY || currentGameDay == DayOfWeek.SATURDAY) {
+					loggerUtils.log("info", "Creating weekend run, start time={}", gameStart);
+					createWeekendSchedule(dflRound, gameStart);
 				} else {
-					loggerUtils.log("info", "Creating weekday run, start time={}", startTimeCal);
-					createWeekdaySchedule(dflRound, startTimeCal);
+					loggerUtils.log("info", "Creating weekday run, start time={}", gameStart);
+					createWeekdaySchedule(dflRound, gameStart);
 				}
 				
 				previousGameDay = currentGameDay;
@@ -119,37 +124,51 @@ public class ResultsJobGenerator {
 			
 		}
 		
-		loggerUtils.log("info", "Creating final run, start time={}", startTimeCal);
-		createFinalRunSchedule(dflRound, startTimeCal);
+		loggerUtils.log("info", "Creating final run, start time={}", gameStart);
+		createFinalRunSchedule(dflRound, gameStart);
 	}
 	
-	private void createWeekendSchedule(int dflRound, Calendar time) throws Exception {
-		time.set(Calendar.HOUR_OF_DAY, 19);
-		time.set(Calendar.MINUTE, 0);
+	private void createWeekendSchedule(int dflRound, ZonedDateTime time) throws Exception {
+		//time.set(Calendar.HOUR_OF_DAY, 19);
+		//time.set(Calendar.MINUTE, 0);
+		time = time.withHour(19);
+		time = time.withMinute(0);
 		scheduleJob(dflRound, false, time);
 			
-		time.set(Calendar.HOUR_OF_DAY, 23);
-		time.set(Calendar.MINUTE, 0);
+		//time.set(Calendar.HOUR_OF_DAY, 23);
+		//time.set(Calendar.MINUTE, 0);
+		time = time.withHour(23);
+		time = time.withMinute(0);
 		scheduleJob(dflRound, false, time);	
 	}
 	
-	private void createWeekdaySchedule(int dflRound, Calendar time) throws Exception {
-		time.set(Calendar.HOUR_OF_DAY, 23);
-		time.set(Calendar.MINUTE, 0);
+	private void createWeekdaySchedule(int dflRound, ZonedDateTime time) throws Exception {
+		//time.set(Calendar.HOUR_OF_DAY, 23);
+		//time.set(Calendar.MINUTE, 0);
+		time = time.withHour(23);
+		time = time.withMinute(0);
 		scheduleJob(dflRound, false, time);	
 	}
 	
-	private void createFinalRunSchedule(int dflRound, Calendar time) throws Exception {
-		time.set(Calendar.DAY_OF_MONTH, time.get(Calendar.DAY_OF_MONTH)+1);
-		time.set(Calendar.HOUR_OF_DAY, 9);
-		time.set(Calendar.MINUTE, 0);
+	private void createFinalRunSchedule(int dflRound, ZonedDateTime time) throws Exception {
+		//time.set(Calendar.DAY_OF_MONTH, time.get(Calendar.DAY_OF_MONTH)+1);
+		//time.set(Calendar.HOUR_OF_DAY, 9);
+		//time.set(Calendar.MINUTE, 0);
+		time = time.plusDays(1);
+		time = time.withHour(9);
+		time = time.withMinute(0);
 		scheduleJob(dflRound, true, time);	
 	}
 	
-	private void scheduleJob(int round, boolean isFinal, Calendar time) throws Exception {
+	private void scheduleJob(int round, boolean isFinal, ZonedDateTime time) throws Exception {
 		CronExpressionCreator cronExpression = new CronExpressionCreator();
-		cronExpression.setTime(timeFormat.format(time.getTime()));
-		cronExpression.setStartDate(dateFormat.format(time.getTime()));
+		//cronExpression.setTime(timeFormat.format(time.getTime()));
+		//cronExpression.setStartDate(dateFormat.format(time.getTime()));
+		
+		ZonedDateTime timeUtc = time.withZoneSameInstant(ZoneId.of("UTC"));
+		
+		cronExpression.setTime(timeUtc.format(DateTimeFormatter.ofPattern("hh:mm a")));
+		cronExpression.setStartDate(timeUtc.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
 		
 		Map<String, Object> jobParams = new HashMap<>();
 		jobParams.put("ROUND", round);
