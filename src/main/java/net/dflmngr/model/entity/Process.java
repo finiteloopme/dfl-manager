a7cecf2a8fd69af9d7a9fe28c5a7a17da08f0231
Keith Lockwood <kwlockwo@gmail.com>
2017-03-24 09:02:55 +1100

    Fixes for raw stats handler

--- src/main/java/net/dflmngr/model/entity/Process.java
+++ src/main/java/net/dflmngr/model/entity/Process.java
@@ -2,6 +2,7 @@ package net.dflmngr.model.entity;
 
 import java.time.ZonedDateTime;
 
+import javax.persistence.Cacheable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -11,6 +12,7 @@ import javax.persistence.Table;
 import net.dflmngr.model.entity.keys.ProcessPK;
 
 @Entity
+@Cacheable(false)
 @Table(name="process")
 @IdClass(ProcessPK.class)
 public class Process {
