1a1e6b01149bdf7d90a7c2dc93d530652e64ea96
Keith Lockwood <kwlockwo@gmail.com>
2017-03-25 02:19:45 +1100

    Email Send Fixes

--- src/main/java/net/dflmngr/utils/EmailUtils.java
+++ src/main/java/net/dflmngr/utils/EmailUtils.java
@@ -71,7 +71,7 @@ public class EmailUtils {
 
 		//Transport.send(message);
 		String oauthToken = AccessTokenFromRefreshToken.getAccessToken();
-		Transport smptTransport = OAuth2Authenticator.connectToSmtp(outgoingMailHost, outgoingMailPort, mailUsername, oauthToken, false);
+		Transport smptTransport = OAuth2Authenticator.connectToSmtp(outgoingMailHost, outgoingMailPort, mailUsername, oauthToken, true);
 		smptTransport.sendMessage(message, message.getAllRecipients());
 	}
 	
@@ -115,7 +115,7 @@ public class EmailUtils {
 
 		//Transport.send(message);
 		String oauthToken = AccessTokenFromRefreshToken.getAccessToken();
-		Transport smptTransport = OAuth2Authenticator.connectToSmtp(outgoingMailHost, outgoingMailPort, mailUsername, oauthToken, false);
+		Transport smptTransport = OAuth2Authenticator.connectToSmtp(outgoingMailHost, outgoingMailPort, mailUsername, oauthToken, true);
 		smptTransport.sendMessage(message, message.getAllRecipients());
 	}
 	
